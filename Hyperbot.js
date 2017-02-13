022e4e796ceddccbe3eb0e9451fabe9268b86c1a
Hyperbot-one <hyperbot.one@gmail.com>
2017-02-13 01:26:13 -0800

    v1.5
    Hyperbotv1.5 changes:
    -large refactoring of the code in the way that commands are stored and
    loaded; makes it much easier to add new and more complex commands and
    features in the future.
    -commands are no longer case sensitive.
    -added the ability  to add cheer commands (!cheerAdd name|cheer) please
    keep the use of this command reasonable, if you are found to be abusing
    this feature you're ability to use it will be removed, your commands
    added are being logged. (must have a team related role)
    -added the ability to remove cheers from the cheer list (!cheerRemove
    cheerName) (roles able to use this: Hyper SPAM Queen, Tech Help,
    HyperCat Coach)
    -added a bunch of new commands, check !commands or !spamlist
    -fixed a number of bugs

--- Hyperbot.js
+++ Hyperbot.js
@@ -1,68 +1,119 @@
+
+/*TODO:
+	- !kao banned
+	- !jake/smurg
+	- !roll20 leaderboard
+	- cooldown timer
+	- add/remove txt commands
+*/
+
 const Discord = require("discord.js");
 const config = require("./config.json");
 var fs = require('fs');
 var bot = new Discord.Client();
 var token = config.token;
-//-------------SPAM----------------------------------------------------------------------
-var spam = new Map([
-	["cancer", "calebSHUTTLE HERE COMES :busstop: THE SHUTTLE FeelsGoodMan"],
-	["spamQueen1", "moon2SMAG THC moon2SMAG AKA moon2SMAG TEAM moon2SMAG OF moon2SMAG THE moon2SMAG SPAM moon2SMAG QUEEN moon2SMAG"],
-	["spamQueen2", "moon2XD  THC DOESN'T CARE TO BE SEEN moon2VAPE WE ALREADY GOT OURSELVES A SPAM QUEEN moon2WOOP"],
-	["spamQueen3", "moon2COOL OUR PLAYERS ARE MEAN AND LEAN moon2GASM OUR CHEERLEADERS WILL SUCK YOUR PEEN moon2BANNED OUR MANAGER'S THE SPAM QUEEN moon2CATEMOTE THC'S THE BEST TEAM YOU'VE EVER SEEN moon22"],
-	["globaled", "moon2AWW OUR MASCOT'S CUTE moon2CATEMOTE OUR CHAT IS SPAM moon2MLEM OUR CAPTAIN'S USUALLY moon2SMAG GLOBAL BANNED moon2BANNED"],
-	["THC","moon2SMAG Our rank doesnt matter moon2SMAG because teamwork is key moon2SMUG we are Team HyperCat moon2GOOD aka THC moon2VAPE"],
-	["gold","moon2CATEMOTE  HYPERCAT IS PROUD moon2SMUG HYPERCAT IS BOLD moon2COOL WE GOT GRILLS moon2TEEHEE AND OUR CAPTAIN IS GOLD moon2LUL"],
-	["mustBeTHC","moon2GOOD 3 masters moon2GOOD the rest are plat moon2GOOD must be moon2CATEMOTE Team HyperCat moon2CATEMOTE"]
-]);
-
+var cheerRoles;
+var spamRoles;
 
 //-------------COMMANDS -----------------------------------------------------------------
 var commands = new Map([
-	["help",{process:
-		function(bot, msg){
-			msg.channel.sendMessage("Hi I'm Hyperbot!");
-			msg.channel.sendMessage("I'm here for the meme's and to help you degens cheer on the best team in the Mooncord.");
-			msg.channel.sendMessage("If you want to see what commands I currently respond to please type !commands.");
-			msg.channel.sendMessage("!avatar followed by someone's username or nickname will link you to the full sized discord hosted image file of their avatar (e.g. \"!avatar Hyperbot\"), if you'd like to be added/removed from this functionality type !blacklist add/remove (e.g. \"!blacklist add\" will add you to the blacklist and users will not be able to download your avatar).");
-			msg.channel.sendMessage("I'm programmed in Javascript using the node.js and discord.js frameworks, if you'd like to look at my code type !git");
+	["spamList", {process:
+		function(bot,msg,spam){
+			let spamList = "";
+			for(let txt of Object.keys(spam.txt)){
+				spamList = spamList + "!" + txt + "\n";
+			}
+			msg.channel.sendMessage("```"+spamList+"```")
 	}}],
-	["git", {process:
-		function(bot, msg){
-			msg.channel.sendMessage("https://github.com/Hyperbot-one/Hyperbot");
-	}}],	
-	["ping",{process:
-		function(bot, msg){
-			msg.channel.sendMessage("pong!");
+	["cheerList",{process:
+		function(bot,msg,spam){
+			let cheerList = "";
+			for(let cheer of Object.keys(spam.cheers)){
+				cheerList = cheerList + "!cheer " + cheer + "\n";
+			}
+			msg.channel.sendMessage("```"+cheerList+"```")
 	}}],
-	["lenny",{process:
-		function(bot, msg){
-			msg.channel.sendMessage("( ͡° ͜ʖ ͡°)");
+	["cheerRandom",{process:
+		function(bot, msg, spam){
+			msg.channel.sendMessage(spam.cheers[randomCheer(spam.cheers)]);
 	}}],
-	["justright",{process:
-		function(bot, msg){
-			msg.channel.sendMessage("✋😩👌");
+	["acc",{process:
+		function(bot, msg, spam){
+			let x = msg.content.slice(1).split(" ").slice(1).join(" ");
+			msg.channel.sendMessage("Im AcceptableLosses and " + x);
 	}}],
-	["tableflip", {process:
-		function(bot, msg){
-			msg.channel.sendMessage("(╯°□°）╯︵ ┻━┻");
+	["cheerAdd",{process:
+		function(bot,msg,spam,approvedRoles){
+			let cheer = msg.content.slice(1).split(" ").slice(1).join(" ").split("|");
+			let cheerName = cheer[0];
+			let cheerTxt = cheer[1];
+			let approved = false;
+			for(let role of approvedRoles){
+				if(role.members.has(msg.author.id)){
+					approved = true;
+				}
+			}
+			if(approved){
+				if(searchCheers(cheerName,getJSON('./spam.json'))){
+					msg.channel.sendMessage("a cheer with this name already exists");
+					return;
+				}
+				spam.cheers[cheerName] = cheerTxt;
+				writeSpam(spam.cheers, spam.txt);
+				if(searchCheers(cheerName,getJSON('./spam.json'))){
+					msg.channel.sendMessage("your cheer has successfully been added to the list");
+					console.log(msg.author.username + " has added cheer- "+ cheer[0] +" : "+ cheer[1]);
+				} else {
+					msg.channel.sendMessage("your cheer has not been added to the list, make sure you are correctly formating the command ex. `!cheerAdd name|cheer`");
+				}
+			}else{
+				msg.channel.sendMessage("Sorry you dont have permision to use this command");
+			}
 	}}],
-	["unflip", {process:
-		function(bot, msg){
-			msg.channel.sendMessage("┬──┬﻿ ノ( ゜-゜ノ)");
+	["cheerRemove", {process: 
+		function(bot,msg,spam,approvedRoles){
+			let cheerName = msg.content.slice(1).split(" ").slice(1);
+			let approved = false;
+			for(let role of approvedRoles){
+				if(role.members.has(msg.author.id)){
+					approved = true;
+				}
+			}
+			if(approved){
+				if(!searchCheers(cheerName,getJSON('./spam.json'))){
+					msg.channel.sendMessage(cheerName + " is not a listed cheer");
+					return;
+				}
+				delete spam.cheers[cheerName];
+				writeSpam(spam.cheers, spam.txt);
+				if(!searchCheers(cheerName,getJSON('./spam.json'))){
+					msg.channel.sendMessage(cheerName +" has been deleted");
+					console.log(msg.author.username + " has removed " + cheerName);
+				} else {
+					msg.channel.sendMessage(cheerName + "was not removed from the list, try again or contact kdubious");
+				}
+			}else{
+				msg.channel.sendMessage("Sorry you dont have permision to use this command");
+			}
 	}}],
-	["selfDestruct", {process:
-		function(bot, msg){
-			msg.channel.sendMessage("!selfDestruct");
-			setTimeout(() => { msg.channel.sendMessage("!selfDestruct")}, 1000);
-			setTimeout(() => { msg.channel.sendMessage("!selfDestruct")},2000);
-			setTimeout(() => { msg.channel.sendMessage("!selfDestruct")}, 3000);
-			setTimeout(() => { msg.channel.sendMessage("!selfDestruct")}, 4000);
-			setTimeout(() => { msg.channel.sendMessage("BOOM Troll1")}, 5000);
-			setTimeout(() => { msg.channel.sendMessage("testing timing of functions and that the bot can't trigger itself")}, 5500);
+	["roll20", {process:
+		function(bot,msg){
+			let i = randomIntBtw(1,21);
+			if(i == 20){
+				msg.channel.sendMessage(msg.author + " PogChamp you rolled " + i + "! Watch out we have a bad ass over here BadAss https://www.youtube.com/watch?v=6RJgpPyadeM");
+			} else if(i < 20 && i >= 15){
+				msg.channel.sendMessage(msg.author + " you rolled " + i + " moon2OOO Not too bad! https://media.makeameme.org/created/well-done-you-htk6a4.jpg");
+			} else if(i < 15 && i > 5){
+				msg.channel.sendMessage(msg.author + " you rolled " + i + ", congrats you're mediocre FeelsBadMan");
+			} else if(i <= 5 && i > 1){
+				msg.channel.sendMessage(msg.author + " you rolled " + i + ", at least you tried FailFish https://s-media-cache-ak0.pinimg.com/736x/a7/92/e7/a792e77615f449b39f173fc0c165ada5.jpg");
+			} else {
+				msg.channel.sendMessage(msg.author + " you rolled " + i + "! LOLguy git gud scrub https://www.youtube.com/watch?v=2-CnmVVHIzU");
+			}
 	}}],
 	["avatar", {process:
 		function(bot, msg){
-			if(searchBL(msg.author.id, getBL()) == -1){
+			if(searchBL(msg.author.id, getJSON('./blacklist.json')) == -1){
 				let userName = msg.content.slice(1).split(" ").slice(1).join(" ");
 				let userPic = bot.users.find('username', userName);
 				if(userPic == null){
@@ -86,7 +137,7 @@ var commands = new Map([
 			if(mod == "add"){
 				console.log("mod: add");
 				add2BL(userID);
-				if(searchBL(userID, getBL()) != -1){
+				if(searchBL(userID, getJSON('./blacklist.json')) != -1){
 					msg.reply("You've been added to the !avatar blacklist, to be removed type \"!blacklist remove\"");
 				} else {
 					msg.reply("There was an error adding you to the blacklist, please try again or contact kdubious");
@@ -94,7 +145,7 @@ var commands = new Map([
 			}else if(mod == "remove"){
 				console.log("mod: remove");
 				removeFromBL(userID);
-				if(searchBL(userID, getBL()) == -1){
+				if(searchBL(userID, getJSON('./blacklist.json')) == -1){
 					msg.reply("You've been removed from the !avatar blacklist, to be added type \"!blacklist add\"");
 				} else {
 					msg.reply("There was an error removing you from the blacklist, please try again or contact kdubious");
@@ -107,45 +158,71 @@ var commands = new Map([
 				msg.channel.sendMessage("please type \"!blacklist add\" to be added to the !avatar blacklist, or \"!blacklist remove\" to be removed and allow !avatar commands with your name.");
 			}		
 	}}],
-	["cheer",{process:
+	["selfDestruct", {process:
 		function(bot, msg){
-			let mod = msg.content.slice(1).split(" ")[1];
-			if(spam.has(mod)){
-				msg.channel.sendMessage(spam.get(mod));
-			} else {
-				msg.channel.sendMessage(mod + " is not a valid cheer, to view the list of cheers type !cheerList");
-			}
-	}}],
-	["cheerList",{process:
-		function(bot, msg){
-			for(let cheer of spam.keys()){
-				msg.channel.sendMessage("!cheer " + cheer);
-			}
-	}}],
-	["cheerRandom",{process:
-		function(bot, msg){
-			msg.channel.sendMessage(spam.get(randomCheer()));
+			msg.channel.sendMessage("!selfDestruct");
+			setTimeout(() => { msg.channel.sendMessage("!selfDestruct")}, 1000);
+			setTimeout(() => { msg.channel.sendMessage("!selfDestruct")},2000);
+			setTimeout(() => { msg.channel.sendMessage("!selfDestruct")}, 3000);
+			setTimeout(() => { msg.channel.sendMessage("!selfDestruct")}, 4000);
+			setTimeout(() => { msg.channel.sendMessage("***BOOM*** Troll1")}, 5000);
+			setTimeout(() => { msg.channel.sendMessage("testing timing of functions and that the bot can't trigger itself")}, 5500);
 	}}],
 	["commands", {process:
 		function(bot,msg){
 			let commandList = "";
+			commandList = commandList + "!help\n";
+			commandList = commandList + "!git\n";
 			for (var [key, value] of commands){
 				if(key == "avatar"){
-					commandList = commandList + "!" + key + " (username/nickname)" + "\n";
+					commandList = commandList + "!" + key + " (username/nickname)\n";
 				} else if(key == "blacklist"){
-					commandList = commandList + "!" + key + " (add/remove)" + "\n";
+					commandList = commandList + "!" + key + " (add/remove)\n";
+				} else if(key == "cheerAdd"){
+					commandList = commandList + "!" + key + " cheerName|cheer\n";
+				} else if(key == "cheerRemove"){
+					commandList = commandList + "!" + key + " cheerName\n";
 				} else {
 					commandList = commandList + "!" + key + "\n";
 				}
 			}
-			msg.channel.sendMessage(commandList);
+			
+			msg.channel.sendMessage("```"+commandList+"```");
 	}}]
 ]);
 //--------------------------END COMMANDS-----------------------------------------------------
 
+function writeSpam(cheersMap, txtMap){
+	var spam = getJSON('./spam.json');
+	spam.cheers = cheersMap;
+	spam.txt = txtMap;
+	var json = JSON.stringify(spam);
+	fs.writeFileSync('./spam.json', json);	
+}
+
+function searchCheers(key,spam){
+	for(let cheer of Object.keys(spam.cheers)){
+		console.log("test");
+		if(cheer == key){
+			return true;
+		}
+	}
+	return false;
+}
+
+
+function searchTxt(key,spam){
+	for(let cheer of Object.keys(spam.txt)){
+		if(cheer == key){
+			return true;
+		}
+	}
+	return false;
+}
+
 //adds a user to the blacklist array and saves to blacklist.json
 function add2BL(userID){
-	var bl = getBL();
+	var bl = getJSON('./blacklist.json');
 	if(searchBL(userID, bl) == -1){
 		bl.push(userID);
 		writeBL(bl, userID, "add");
@@ -154,7 +231,7 @@ function add2BL(userID){
 
 //removes a user to the blacklist array and saves to blacklist.json
 function removeFromBL(userID){
-	var bl = getBL();
+	var bl = getJSON('./blacklist.json');
 	var i = searchBL(userID, bl);
 	if(i >= 0){
 		bl.splice(i,1);
@@ -171,26 +248,15 @@ function searchBL(userID, bl){
 }
 
 function printBL(ch){
-	var bl = getBL();
+	var bl = getJSON('./blacklist.json');
 	for(var i = 0; i < bl.length; i++){
 		ch.sendMessage(bl[i] + " ");
 	}
 }
 
-
-//parses the blacklist.json file to a js array containing userID's of users who want to be excluded from the !avatar feature
-function getBL(){
-	var bl = JSON.parse(fs.readFileSync('./blacklist.json', 'utf8'));
-	/*fs.readFile('./blacklist.json', 'utf8', function (err, data) {
-		if(err) throw err;
-		bl = JSON.parse(data);
-		console.log(bl)
-	});*/
-	console.log("reading blacklist.json:");
-	for(var i = 0; i < bl.length; i++){
-		console.log("	" + bl[i]);
-	}
-	return bl;
+//returns the javascript equivalent object from the provided .json file 
+function getJSON(path){
+	return JSON.parse(fs.readFileSync(path, 'utf8'));
 }
 
 //writes the blacklist array object to blacklist.json
@@ -206,13 +272,32 @@ function writeBL(bl, userID, modType){
 	});*/
 }
 
-function randomCheer(){
+function randomIntBtw(min, max){
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random()*(max - min)) + min;	
+}
+
+function randomCheer(cheerMap){
 	var keysArr = [];
-	for(let key of spam.keys()){
+	for(let key of Object.keys(cheerMap)){
 		keysArr.push(key);
 	}
-	console.log(keysArr[Math.floor(Math.random()*(keysArr.length))]);
-	return keysArr[Math.floor(Math.random()*(keysArr.length))];
+	return keysArr[randomIntBtw(0,keysArr.length)];
+}
+
+function getLowerCaseKeyMap(obj, map){
+	var lk = new Map();
+	if(map){
+		for(var x of obj.keys()){
+			lk.set(x.toLowerCase(), x);
+		}
+	}else{
+		for(var x of Object.keys(obj)){
+			lk.set(x.toLowerCase(), x);
+		}
+	}
+	return lk;	
 }
 
 //--------------------------EVENTS--------------------------------------------------------------------------------
@@ -223,33 +308,64 @@ bot.on("message", msg => {
 	if(!msg.content.startsWith(prefix)) return;
 	if(msg.author.bot) return;
 	
-	var cmd = msg.content.slice(1).split(" ");
-	console.log(cmd[0]);
-	cmd = cmd[0];
-	
-	//any messages in the delta group server is limited to the #discord_bots_testground channel
-	if (msg.guild.id == "226465786871414784"){
-		if (msg.channel.id != "257759410384797697" ){
-			msg.channel.sendMessage("get back in #discord_bots_testground Kapp");
-			return;
-		}
-	}
-	
-	//limits hyperbot to #hyperbot-factory in Hypercats server for testing
+	/*limits hyperbot to #hyperbot-factory in Hypercats server for testing
 	if (msg.guild.id == "154419834728218625"){
 		if (msg.channel.id != "279104520104050688" ){
 			return;
 		}
-	}
+	}*/
+	var spam = getJSON('./spam.json');
 	
-	//takes the first word of the message and searches the commands Map, if it is a valid command it will run the coresponding function
-	if (commands.has(cmd)){
-		commands.get(cmd).process(bot,msg);
+	var cmdKeys = getLowerCaseKeyMap(commands, true);
+	var cheerKeys = getLowerCaseKeyMap(spam.cheers, false);
+	var txtKeys = getLowerCaseKeyMap(spam.txt, false);
+
+	var command = msg.content.slice(1).split(" ");
+	console.log(command[0]);
+	var cmd = command[0].toLowerCase();
+	if(command[1]){	var cheer = command[1].toLowerCase();}
+	
+	//takes the first word of the message and searches the commands Map, the cheers map, and the txt map, if it is a valid command it will run the coresponding function
+	if (cmdKeys.has(cmd)){
+		if(cmd == "cheerlist" || cmd == "cheerrandom" || cmd == "spamlist"){
+			commands.get(cmdKeys.get(cmd)).process(bot,msg,spam);
+		}else if(cmd == "cheeradd"){
+			commands.get(cmdKeys.get(cmd)).process(bot,msg,spam,cheerAddRoles);
+		}else if(cmd == "cheerremove"){
+			commands.get(cmdKeys.get(cmd)).process(bot,msg,spam,cheerRemoveRoles);
+		}else{
+			commands.get(cmdKeys.get(cmd)).process(bot,msg);
+		}
+	}else if(cmd == "cheer" && cheerKeys.has(cheer)){
+		msg.channel.sendMessage(spam.cheers[cheerKeys.get(cheer)]);
+	}else if(txtKeys.has(cmd)){
+		msg.channel.sendMessage(spam.txt[txtKeys.get(cmd)]);
+	}
+});
+
+bot.on('guildMemberAdd',user =>{
+	var thc = bot.servers.get('273241020077047810');
+    if(user.guild == thc){
+		bot.channels.get('278395091641565196').sendMessage(user.user.username + " has joined the server");
 	}
 });
 
 bot.on('ready', () => {
-  console.log(`Ready to server in ${bot.channels.size} channels on ${bot.guilds.size} servers, for a total of ${bot.users.size} users.`);
+	console.log(`Ready to server in ${bot.channels.size} channels on ${bot.guilds.size} servers, for a total of ${bot.users.size} users.`);
+	thc = bot.guilds.get('154419834728218625');
+	cheerAddRoles = [
+		thc.roles.get('275378439031095316'),	//Hyper SPAM Queen
+		thc.roles.get('278394315770822656'),	//Tech Help
+		thc.roles.get('276489543853801483'),	//Team HyperCat
+		thc.roles.get('278387425850556417'),	//HyperCat Coach
+		thc.roles.get('276488938456219659'),	//HyperKittens
+	];
+	
+	cheerRemoveRoles = [
+		thc.roles.get('275378439031095316'),	//Hyper SPAM Queen
+		thc.roles.get('278394315770822656'),	//Tech Help
+		thc.roles.get('278387425850556417'),	//HyperCat Coach
+	];
 });
 //--------------------------END EVENTS----------------------------------------------------------------------------
 bot.login(token);
